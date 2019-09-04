943c9a5216e1a6666b7f944ff83b727fd4a7aeed
Tom Crosley <tom.crosley@microchip.com>
2019-09-04 15:58:41 -0700

    [ble_comm] replace bm64_ble_comm with ble_comm, add five new variants: MZEF C2+BM64, MZEF C2+BM71, E54 CULT+BM71, E54 XPRO+BM71, and D21 XPRO+BM71 MH3-21945 MH3-21946 MH3-21947 MH3-21948 MH3-21949

---
+++ apps/data/ble_comm/firmware/src/app.c
@@ -0,0 +1,290 @@
+/*******************************************************************************
+  MPLAB Harmony Application Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    app.c
+
+  Summary:
+    This file contains the source code for the MPLAB Harmony application.
+
+  Description:
+    This file contains the source code for the MPLAB Harmony application.  It
+    implements the logic of the application's state machine and it may call
+    API routines of other MPLAB Harmony modules in the system, such as drivers,
+    system services, and middleware.  However, it does not call any of the
+    system interfaces (such as the "Initialize" and "Tasks" functions) of any of
+    the modules in the system or make any assumptions about when those functions
+    are called.  That is the responsibility of the configuration-specific system
+    files.
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <string.h>
+#include "app.h"        // also beings in app_tone_lookup_table.h
+#include "ble.h"
+extern BLE_DATA bleData;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Application Data
+
+  Summary:
+    Holds application data
+
+  Description:
+    This structure holds the application's data.
+
+  Remarks:
+    This structure should be initialized by the APP_Initialize function.
+
+    Application strings and buffers are be defined outside this structure.
+*/
+
+APP_DATA appData;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Callback Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/// *****************************************************************************
+ /*
+  Function:
+        static void App_TimerCallback
+        (
+            uintptr_t context
+        )
+
+  Summary:
+    Implements the handler for timer callback function.
+
+  Description:
+    Called every 1 ms by timer services.  It then decrements WM8904Delay if
+    non-zero.
+
+  Parameters:
+    context      - Contains index into driver's instance object array
+
+  Remarks:
+    None
+*/
+static void App_TimerCallback( uintptr_t context)
+{
+    if (appData.buttonDelay)
+    {      
+        appData.buttonDelay--;
+    }
+    
+    if (appData.waitingToConnect)
+    {
+        appData.waitingToConnectTimer++;
+        if ((appData.waitingToConnectTimer & 0xFF) == 0)    // about every 1/4 second
+        {
+            LED1_Toggle();
+        }
+    }
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Local Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* TODO:  Add any necessary local functions.
+*/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Initialization and State Machine Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************************************
+  Function:
+    void APP_Initialize ( void )
+
+  Remarks:
+    See prototype in app.h.
+ */
+
+void APP_Initialize ( void )
+{
+    /* TODO: Initialize your application's state machine and other
+     * parameters.
+     */   
+
+	LED1_On();       // initially on, until connected
+    appData.state = APP_STATE_INIT;   
+ 
+    appData.buttonState = BUTTON_STATE_IDLE;   
+    appData.buttonDelay = 0;        // used for button debounce
+    appData.buttonMode = false;
+    appData.queryDelay = 0;         // used for status queries
+
+    appData.waitingToConnect = false;
+    appData.waitingToConnectTimer = 0;
+
+    bleInitialize(true);
+}
+
+/******************************************************************************
+  Function:
+    void APP_Tasks ( void )
+
+  Remarks:
+    See prototype in app.h.
+ */
+DRV_HANDLE tmrHandle;
+
+void APP_Tasks ( void )
+{    
+    bleTasks(); 
+    
+    /* Check the application's current state. */
+    switch ( appData.state )
+    {       
+        /* Application's initial state. */
+        case APP_STATE_INIT:
+        {
+            /* Open the timer Driver */
+            tmrHandle = SYS_TIME_CallbackRegisterMS(App_TimerCallback, 
+                    (uintptr_t)0, 1/*ms*/, SYS_TIME_PERIODIC);
+
+            if ( SYS_TIME_HANDLE_INVALID != tmrHandle )
+            {
+               appData.state = APP_STATE_WAIT_INIT;
+            }            
+        }
+        break;
+        
+        case APP_STATE_WAIT_INIT:
+        {
+            if (DRV_BT_STATUS_READY == DRV_BT_GetPowerStatus())
+            {           
+                appData.state=APP_STATE_IDLE;
+            }
+        }
+        break;
+        
+        // Initialized 
+        case APP_STATE_IDLE:
+        {
+            APP_Button_Tasks();
+            
+            if (appData.queryDelay == 0)
+            {
+                DRV_BT_BLE_QueryStatus(bleData.bt.handle);
+                appData.queryDelay = QUERY_DELAY;
+            }
+		}
+		break;
+         
+        default:
+        {
+            /* TODO: Handle error in application's state machine. */
+        }
+        break;             
+    }
+}
+
+#define BUTTON_DEBOUNCE 50
+#define LONG_BUTTON_PRESS 1000
+
+void APP_Button_Tasks()
+{
+    char* test_string;
+        
+   //BUTTON PROCESSING
+    /* Check the buttons' current state. */      
+    switch ( appData.buttonState )
+    {
+        case BUTTON_STATE_IDLE:
+        {
+            if ( (appData.buttonDelay==0)&&
+                 (SWITCH_Get()==SWITCH_STATE_PRESSED))                
+            {
+                appData.buttonDelay=BUTTON_DEBOUNCE;       
+                appData.buttonState=BUTTON_STATE_PRESSED;               
+            }
+        }
+        break;
+        
+        case BUTTON_STATE_PRESSED:
+        { 
+            if (appData.buttonDelay>0)
+            {
+                break;      // still debouncing
+            }
+            
+            if(SWITCH_Get()==SWITCH_STATE_PRESSED) 
+            {                
+                appData.buttonDelay=LONG_BUTTON_PRESS;          // 1 sec is long press
+                appData.buttonState=BUTTON_STATE_BUTTON0_PRESSED;                  
+            }
+        }
+        break;
+          
+        case BUTTON_STATE_BUTTON0_PRESSED:
+        {
+            if ((appData.buttonDelay>0)&&
+                (SWITCH_Get()!=SWITCH_STATE_PRESSED))     // SW01 pressed and released < 1 sec
+            {
+                if (appData.buttonMode)
+                {
+	                // future feature    
+                }
+                else
+                {
+	                test_string = "Button pressed";
+	                DRV_BT_SendDataOverBLE(bleData.bt.handle, (uint8_t*)test_string, strlen(test_string));
+                }
+                appData.buttonDelay=BUTTON_DEBOUNCE;                
+                appData.buttonState=BUTTON_STATE_IDLE;              
+            }
+            else if ((appData.buttonDelay==0)&&
+                     (SWITCH_Get()==SWITCH_STATE_PRESSED))  // SW0 still pressed after 1 sec
+            {
+                // long press reserved for future feature
+                //appData.buttonMode = !appData.buttonMode;             
+                
+                appData.buttonState=BUTTON_STATE_WAIT_FOR_RELEASE;                
+            }                          
+        } 
+        break;
+
+        case BUTTON_STATE_WAIT_FOR_RELEASE:
+        {
+            if (SWITCH_Get()!=SWITCH_STATE_PRESSED)
+            {
+                appData.buttonDelay=BUTTON_DEBOUNCE;
+                appData.buttonState=BUTTON_STATE_IDLE;
+            }
+        }
+
+        /* The default state should never be executed. */
+        default:
+        {
+            /* TODO: Handle error in application's state machine. */
+            break;
+        }
+    }
+}
+
+/*******************************************************************************
+ End of File
+ */
---
+++ apps/data/ble_comm/firmware/src/config/sam_e54_xpro_bm71/bt/driver/bm64/drv_bm64_uart.c
@@ -0,0 +1,175 @@
+/*******************************************************************************
+  BM64 Bluetooth Static Driver implementation
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    drv_bm64_uart.c
+
+  Summary:
+   BM64 Bluetooth Static Driver source file for UART functions.
+
+  Description:
+    This file is the implementation of the internal functions of the BM64
+    driver related to sending and receiving data to/from the BM64 over UART.
+ 
+*******************************************************************************/
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files 
+// *****************************************************************************
+// *****************************************************************************
+
+#include "definitions.h"
+#include "bt/driver/bm64/drv_bm64.h"
+#include "bt/driver/bm64/drv_bm64_local.h"
+#include "bt/driver/bm64/drv_bm64_command_send.h"
+#include "bt/driver/bm64/drv_bm64_uart.h"
+
+// all #defines, enums and non-static functions and variables prefixed by
+// DRV_BM64 to avoid name conflicts
+
+static DRV_BM64_UART_DATA uartData;
+volatile uint8_t DRV_BM64_eusartRxCount;
+volatile uint8_t rxData;
+
+// called from DRV_BM64_Initialize, not the directly by the user
+void DRV_BM64_UART_Initialize ( void )
+{
+
+    /* Place the App state machine in its initial state. */
+    uartData.state = DRV_BM64_UART_STATE_INIT;
+    
+    uartData.rxHead = 0; 
+    
+    uartData.rxTail = 0;
+    
+    DRV_BM64_eusartRxCount=0;  
+}
+
+
+/******************************************************************************
+  Function:
+    void UartWriteBytesData ( uint8_t* pTxBuffer, uint16_t size )
+
+  Return:
+    true - if actual number of bytes transmitted is same as the argument size.
+    false - in case of an error
+ 
+  Remarks:
+    
+ */
+
+
+void DRV_BM64_EUSART_Write(uint8_t txData)
+{ 
+    SERCOM7_USART_Write(&txData,1);  
+}
+
+uint8_t DRV_BM64_EUSART_Read(void)
+{
+    uint8_t readByte;
+    
+    readByte=uartData.rxBuffer[uartData.rxTail++];
+    
+    if(uartData.rxTail >= RX_BUFFER_SIZE)
+    {
+        uartData.rxTail = 0;
+    }
+   
+    DRV_BM64_eusartRxCount--;
+    
+    return readByte;
+    
+}
+
+void DRV_BM64_UART_TransmitEventHandler(uintptr_t context)
+{
+     
+    DRV_BM64_UART_TransferNextByte();
+
+}
+
+void DRV_BM64_UART_ReceiveEventHandler(uintptr_t context)
+{    
+    uartData.rxBuffer[uartData.rxHead++] = rxData;
+   SERCOM7_USART_Read((void*)&rxData, 1);    // set up first read
+
+    if(uartData.rxHead >= RX_BUFFER_SIZE)
+    {
+        uartData.rxHead = 0;
+    }
+    
+    DRV_BM64_eusartRxCount++;
+}
+/******************************************************************************
+  Function:
+    void UART_Tasks ( void )
+
+  Remarks:
+    See prototype in app.h.
+ */
+
+ void DRV_BM64_UART_Tasks ( void )
+{
+    /* Check the application's current state. */
+    switch ( uartData.state )
+    {
+        /* Application's initial state. */
+        case DRV_BM64_UART_STATE_INIT:
+        
+            // Register an event handler with driver. This is done once
+           SERCOM7_USART_WriteCallbackRegister(DRV_BM64_UART_TransmitEventHandler, 0);
+           SERCOM7_USART_ReadCallbackRegister(DRV_BM64_UART_ReceiveEventHandler, 0);
+
+           SERCOM7_USART_Read((void*)&rxData, 1);    // set up first read
+            
+            uartData.state = DRV_BM64_UART_STATE_TRANSMIT;           
+
+            break;
+            
+        case DRV_BM64_UART_STATE_TRANSMIT:
+            if(!SERCOM7_USART_WriteIsBusy())
+			{
+                DRV_BM64_UART_TransferNextByte();
+			}
+
+            break;
+        
+
+        /* The default state should never be executed. */
+        default:        
+            /* TODO: Handle error in application's state machine. */
+            break;        
+    }
+}
+
+/*******************************************************************************
+ End of File
+ */
